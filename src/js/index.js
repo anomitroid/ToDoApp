0fb60ead25e790ab19d31cb19b861028ded73fb8
anomitroid <anomitroid2005@gmail.com>
2024-12-19 01:10:52 +0530

    used raw-loader

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,17 @@ import createSidebar from "./components/sidebar";
 import navigator from "./utils/navigator";
 import tasks from "./store/tasks";
 import "./store/autoDelete";
+import siteIcon from "!!raw-loader!C:/Users/anomi/OneDrive/Desktop/HTML/TodoApp/src/assets/icons/check-all.svg";
+
+let link = document.querySelector ("link[rel ~= 'icon']");
+if (!link) {
+    link = document.createElement ("link");
+    link.rel = "icon";
+    document.head.appendChild (link);
+}
+const blob = new Blob ([siteIcon], { type : "image/svg+xml" });
+const blobURL = URL.createObjectURL (blob);
+link.href = blobURL;
 
 createSidebar ();
 createMainSection ();
